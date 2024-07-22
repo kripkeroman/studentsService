340ce9ad7b774e8886d93b486d0e2d18394e50a2
r.kirilov <r.kirilov@w1.money>
2024-07-22 20:57:03 +0300

    added new documentations and setting application.properties

--- src/main/java/org/student/repository/StudentRepository.java
+++ src/main/java/org/student/repository/StudentRepository.java
@@ -16,26 +16,54 @@ public class StudentRepository
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    /**
+     * Add new student
+     *
+     * @param student
+     */
     public void addStudent(Student student)
     {
         String sql = "INSERT INTO students (first_name, last_name, middle_name, date_of_birth, student_group) VALUES (?, ?, ?, ?, ?)";
         jdbcTemplate.update(sql, student.getFirstName(), student.getLastName(), student.getMiddleName(), student.getDateOfBirth(), student.getGroup());
     }
 
+    /**
+     * Delete student by ID
+     *
+     * @param id
+     */
     public void deleteStudentById(Long id)
     {
         String sql = "DELETE FROM students WHERE id = ?";
         jdbcTemplate.update(sql, id);
     }
 
+    /**
+     * Get all students
+     *
+     * @return
+     */
     public List<Student> getAllStudents()
     {
         String sql = "SELECT * FROM students";
         return jdbcTemplate.query(sql, new StudentRowMapper());
     }
 
+    /**
+     * StudentRowMapper is a static inner class that implements the RowMapper interface
+     * for mapping rows of a ResultSet to instances of the Student class.
+     *
+     */
     private static class StudentRowMapper implements RowMapper<Student>
     {
+        /**
+         * Maps a single row of a ResultSet to an instance of the Student class.
+         *
+         * @param rs the ResultSet to map (pre-initialized for the current row)
+         * @param rowNum the number of the current row
+         * @return the Student object for the current row
+         * @throws SQLException if a SQLException is encountered getting column values
+         */
         @Override
         public Student mapRow(ResultSet rs, int rowNum) throws SQLException
         {
